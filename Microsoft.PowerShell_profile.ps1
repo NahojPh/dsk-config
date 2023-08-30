2c1fc5519daff446149ed0986ddce69ca1f0ce1b
NahojPh <44095441+NahojPh@users.noreply.github.com>
2023-08-30 08:42:42 +0200

    Added touch command and cmake cmr & cmb shortcuts as commands

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,17 +1,60 @@
 #Invoke-expression (starship.exe init powershell)
-$env:path += ";C:\Users\0402johphi\cli\helix-22.03-x86_64-windows"
-$env:path += ";C:\Users\0402johphi\cli"
-$env:path += ";C:\Users\0402johphi\cli\jdk-17.0.2\bin"
 
-function dsk {Set-Location "C:\users\0402johphi\Desktop\"}
-Set-Alias -Name hx -Value ha
+# $env:path += ";C:\Users\0402johphi\cli\helix-22.03-x86_64-windows"
+# $env:path += ";C:\Users\0402johphi\cli"
+# $env:path += ";C:\Users\0402johphi\cli\jdk-17.0.2\bin"
+# $env:PICO_SDK_PATH = "C:\Users\johan\libraries\pico-sdk"
+
+function dsk {Set-Location "C:\users\johan\Desktop\"}
+# Set-Alias -Name hx -Value ha
 # Set-Alias -Name lsd -Value "ls | where type == dir"
 # Set-Alias -Name lsf -Value "ls | where type == file"
+Set-Alias -Name c -Value cargo
+
+function touch {
+	param(
+		[String] $name
+	)
+	echo "" > $name
+}
+
+function rcm {
+	param(
+		[String] $out
+	)
+	cd $out
+	cmake ../
+	cd ..
+}
+
+function cmb {
+	param(
+		[String] $d
+	)
+	cmake --build ./$d
+}
+
+
+function cmr {
+	param(
+		[String] $d
+	)
+	cmake --build ./$d
+	Invoke-Expression ./$d/Debug/*.exe
+}
+
+
 function cr {cargo run}
 function ci {cargo init}
 function cb {cargo build}
 function ca {cargo add}
-function pr {Set-Location "C:\Users\0402johphi\projects"}
+function cbr {cargo build --release}
+function pr {Set-Location "C:\Users\johan\projects"}
+function le {Set-Location "C:\Users\johan\latex"}
 function .. {Set-Location ..}
+function q {exit}
+function cwd {echo $pwd | grep -i \\ | clip}
+Set-Alias -Name s -Value scoop
+Set-Alias -Name e -Value explorer
 
 
